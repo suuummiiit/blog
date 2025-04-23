dbb35877b527f11943401ed084f5dce077ccb3fe
sumit <suuummiiit@gmail.com>
2025-04-23 18:13:30 +0530

    templates for home and aritcle_details

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import Home, ArticleDetails
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', Home.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetails.as_view(), name="article_details"),
+
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
 
+class Home(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetails(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+    
