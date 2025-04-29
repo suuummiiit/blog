48fa3468dfa09a0521fbf5169d047d4e95b98f5e
sumit <suuummiiit@gmail.com>
2025-04-29 17:51:27 +0530

    login and registration complete

--- user_auth/views.py
+++ user_auth/views.py
@@ -5,5 +5,5 @@ from django.urls import reverse_lazy
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
-    template = 'registration/registration.html'
+    template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
