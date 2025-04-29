f6bfeecca821c725352149c994da84c8c4c086ff
sumit <suuummiiit@gmail.com>
2025-04-29 22:26:08 +0530

    created model Category and add field category to model Post

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,6 +4,16 @@ from django.urls import reverse
 from datetime import datetime, date
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,6 +21,7 @@ class Post(models.Model):
 
     body = models.TextField()
     post_datetime = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='uncategorized')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
