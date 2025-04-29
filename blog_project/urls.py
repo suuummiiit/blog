7b592d6cb11bf2477f797c6548f41265752430b8
sumit <suuummiiit@gmail.com>
2025-04-29 17:32:04 +0530

    Renamed auth app to user_auth to avoid conflict with django.contrib.auth & registration and login templates

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('auth/', include('auth.urls')),
+    path('user_auth/', include('django.contrib.auth.urls')),
+    path('user_auth/', include('user_auth.urls')),
 ]
